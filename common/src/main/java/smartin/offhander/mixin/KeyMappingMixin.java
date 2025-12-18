bea8e152bdb8f81c37f1fe2a16f77691512d4417
Smartin_b <67115074+smartin-b@users.noreply.github.com>
2025-12-18 06:38:55 +0100

    - fixes and cleaner code

--- common/src/main/java/smartin/offhander/mixin/KeyMappingMixin.java
+++ common/src/main/java/smartin/offhander/mixin/KeyMappingMixin.java
@@ -1,31 +1,48 @@
 package smartin.offhander.mixin;
 
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import net.minecraft.client.KeyMapping;
 import net.minecraft.client.Minecraft;
 import net.minecraft.world.InteractionHand;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import smartin.offhander.OffHanderClient;
 
 @Mixin(KeyMapping.class)
 public class KeyMappingMixin {
 
-    @Inject(method = "Lnet/minecraft/client/KeyMapping;isDown()Z", at = @At("TAIL"), cancellable = true)
-    private void injectMethod(CallbackInfoReturnable<Boolean> cir) {
+    @Shadow
+    private boolean isDown;
+
+    @ModifyReturnValue(method = "Lnet/minecraft/client/KeyMapping;isDown()Z", at = @At("RETURN"))
+    private boolean offhander$injectIsDownConsumeClick(boolean original) {
         KeyMapping mapping = (KeyMapping) (Object) (this);
         if (mapping.equals(Minecraft.getInstance().options.keyUse)) {
-            if(cir.getReturnValue()){
+            if(original){
                 OffHanderClient.ACTIVE_HANDS = new InteractionHand[]{InteractionHand.MAIN_HAND, InteractionHand.OFF_HAND};
             }else if (OffHanderClient.MAIN_HAND.isDown()) {
                 OffHanderClient.ACTIVE_HANDS = new InteractionHand[]{InteractionHand.MAIN_HAND};
-                cir.setReturnValue(true);
+               return true;
             } else if (OffHanderClient.OFF_HAND.isDown()) {
                 OffHanderClient.ACTIVE_HANDS = new InteractionHand[]{InteractionHand.OFF_HAND};
-                cir.setReturnValue(true);
+                return true;
             }
         }
+        return original;
+    }
+
+    @ModifyReturnValue(method = "Lnet/minecraft/client/KeyMapping;consumeClick()Z", at = @At("RETURN"))
+    private boolean offhander$injectConsumeRightClick(boolean original) {
+        KeyMapping mapping = (KeyMapping) (Object) (this);
+        if (mapping.equals(Minecraft.getInstance().options.keyUse) && !isDown) {
+            if (OffHanderClient.MAIN_HAND.isDown()) {
+                return OffHanderClient.MAIN_HAND.consumeClick();
+            } else if (OffHanderClient.OFF_HAND.isDown()) {
+                return OffHanderClient.OFF_HAND.consumeClick();
+            }
+        }
+        return original;
     }
 
 }
--- common/src/main/java/smartin/offhander/mixin/MultiPlayerGameMode.java
+++ common/src/main/java/smartin/offhander/mixin/MultiPlayerGameMode.java
@@ -1,6 +1,5 @@
 package smartin.offhander.mixin;
 
-import net.minecraft.client.Minecraft;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -13,9 +12,6 @@ public interface MultiPlayerGameMode {
     @Inject(method = "Lnet/minecraft/client/multiplayer/MultiPlayerGameMode;releaseUsingItem(Lnet/minecraft/world/entity/player/Player;)V", at = @At("HEAD"), cancellable = true)
     private void injectMethod(CallbackInfo ci) {
         if ((OffHanderClient.MAIN_HAND.isDown() || OffHanderClient.OFF_HAND.isDown())) {
-            //Minecraft.getInstance();
-
-            //instance.releaseUsingItem(player);
             ci.cancel();
         }
     }
